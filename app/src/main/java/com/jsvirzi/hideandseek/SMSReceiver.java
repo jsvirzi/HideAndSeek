6a5cc8e1842af7d53f4a888b38e1767a072f2d78
jsvirzi <jsvirzi@gmail.com>
2016-01-04 02:14:37 -0500

    something started working

--- app/src/main/java/com/jsvirzi/hideandseek/SMSReceiver.java
+++ app/src/main/java/com/jsvirzi/hideandseek/SMSReceiver.java
@@ -13,6 +13,49 @@ import android.widget.Toast;
  * Created by jsvirzi on 1/3/16.
  */
 
+public class SMSReceiver extends BroadcastReceiver {
+
+    // Get the object of SmsManager
+    final SmsManager sms = SmsManager.getDefault();
+
+    public void onReceive(Context context, Intent intent) {
+
+        // Retrieves a map of extended data from the intent.
+        final Bundle bundle = intent.getExtras();
+
+        try {
+
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+
+
+                    // Show Alert
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context,
+                            "senderNum: "+ senderNum + ", message: " + message, duration);
+                    toast.show();
+
+                } // end for loop
+            } // bundle is null
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+
+        }
+    }
+}
+
 //public class SMSReceiver extends BroadcastReceiver {
 //
 //    @Override
@@ -43,44 +86,44 @@ import android.widget.Toast;
 //
 //}
 
-public class SMSReceiver extends BroadcastReceiver {
-
-    public String TAG = "SMSReceiver";
-
-    final SmsManager sms = SmsManager.getDefault();
-
-    public void onReceive(Context context, Intent intent) {
-
-        Log.i(TAG, "Here I am");
-
-        // Retrieves a map of extended data from the intent.
-        final Bundle bundle = intent.getExtras();
-
-        try {
-            if (bundle != null) {
-                final Object[] pdusObj = (Object[]) bundle.get("pdus");
-                for (int i = 0; i < pdusObj.length; i++) {
-                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
-                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
-                    String senderNum = phoneNumber;
-                    String message = currentMessage.getDisplayMessageBody();
-//                    if(message == "SEND GPS") {
-//                        mContext.sendSMSMessage();
-//                        Log.i(TAG, "We are so doing this");
-//                    }
-                    Log.i(TAG, "senderNum: " + senderNum + "; message: " + message);
-                    // Show Alert
-                    int duration = Toast.LENGTH_LONG;
-                    Toast toast = Toast.makeText(context,
-                            "senderNum: "+ senderNum + ", message: " + message, duration);
-                    toast.show();
-
-                } // end for loop
-            } // bundle is null
-
-        } catch (Exception e) {
-            Log.e(TAG, "Exception smsReceiver" +e);
-
-        }
-    }
-}
+//public class SMSReceiver extends BroadcastReceiver {
+//
+//    public String TAG = "SMSReceiver";
+//
+//    final SmsManager sms = SmsManager.getDefault();
+//
+//    public void onReceive(Context context, Intent intent) {
+//
+//        Log.i(TAG, "Here I am");
+//
+//        // Retrieves a map of extended data from the intent.
+//        final Bundle bundle = intent.getExtras();
+//
+//        try {
+//            if (bundle != null) {
+//                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+//                for (int i = 0; i < pdusObj.length; i++) {
+//                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+//                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+//                    String senderNum = phoneNumber;
+//                    String message = currentMessage.getDisplayMessageBody();
+////                    if(message == "SEND GPS") {
+////                        mContext.sendSMSMessage();
+////                        Log.i(TAG, "We are so doing this");
+////                    }
+//                    Log.i(TAG, "senderNum: " + senderNum + "; message: " + message);
+//                    // Show Alert
+//                    int duration = Toast.LENGTH_LONG;
+//                    Toast toast = Toast.makeText(context,
+//                            "senderNum: "+ senderNum + ", message: " + message, duration);
+//                    toast.show();
+//
+//                } // end for loop
+//            } // bundle is null
+//
+//        } catch (Exception e) {
+//            Log.e(TAG, "Exception smsReceiver" +e);
+//
+//        }
+//    }
+//}
